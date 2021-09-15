49213d6e271e8ba94170e704baa4dbbe62010b5f
justice223 <90763038+justice223@users.noreply.github.com>
2021-09-15 19:37:01 +0800

    Add files via upload

---
+++ visual studio daima/demo1/源.cpp
@@ -0,0 +1,18 @@
+//#include<stdio.h>   包含一个叫studio.h的文件
+//int main()          主函数 程序的入口 main的函数有且只有一个
+//{
+//printf("徐梓涵我爱你\n徐梓涵是sb");
+//printf("wdjowdowjd");
+
+//char ch = 'AAAA'; char-字符数据类型
+//printf("%c\n",ch);
+//
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	printf("%d\n",sizeof(float));
+
+}
