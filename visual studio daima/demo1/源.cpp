851a66a58a189d87c8518204eaa548b9dee71658
justice223 <90763038+justice223@users.noreply.github.com>
2021-09-16 22:29:37 +0800

    Add files via upload

--- visual studio daima/demo1/源.cpp
+++ visual studio daima/demo1/源.cpp
@@ -4,15 +4,36 @@
 //printf("徐梓涵我爱你\n徐梓涵是sb");
 //printf("wdjowdowjd");
 
-//char ch = 'AAAA'; char-字符数据类型
-//printf("%c\n",ch);
-//
+//#include<stdio.h>
+//int main()
+//{
+//char ch = 'AAAA'; //char-字符数据类型
+//	printf("ch=%c\n",ch);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf_s("%d%d",&num1,&num2);
+//	sum = num1 + num2;
+//	printf("sum=%d\n",sum);
 //	return 0;
 //}
 
-#include <stdio.h>
-int main()
-{
-	printf("%d\n",sizeof(float));
-
-}
+//#include <stdio.h>
+//enum sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRECT
+//};
+//
+//int main()
+//{
+//	enum sex ch = MALE;
+//	printf("=%c\n",ch);
+//}
